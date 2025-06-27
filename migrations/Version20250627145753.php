702257569c1dc89cfb209505f908ac368e9a74f2
Ivan <vanek.anisimoff@yandex.ru>
2025-06-27 23:10:22 +0700

    Это первый комит для создания ветки

---
+++ migrations/Version20250627145753.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250627145753 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $table = $schema->createTable('profile');
+        $table->addColumn('id', 'integer', ['autoincrement' => true]);
+        $table->addColumn('first_name', 'string', ['notnull' => true]);
+        $table->addColumn('last_name', 'string', ['notnull' => true]);
+        $table->addColumn('email', 'string', ['notnull' => true]);
+        $table->addColumn('phone', 'integer', ['notnull' => true]);
+        $table->addColumn('agreement', 'integer', ['notnull' => true, 'length' => 1]);
+        $table->addColumn('score_bonus', 'integer', ['notnull' => true, 'default' => 0]);
+        $table->setPrimaryKey(['id']);
+    }
+
+    public function down(Schema $schema): void
+    {
+        $schema->dropTable('profile');
+    }
+}
